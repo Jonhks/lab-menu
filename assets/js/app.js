03c947aaec77f337f5a4520e7a13184359d737ed
Jonh <jonathan@laboratoria.la>
2017-09-26 20:37:10 -0500

    agregando condicionales para los tab

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,17 @@
 var mostrarOcultar = function(e){
   console.log(e.target);
-  if(e.target.id === "pestana1"){
+  if(e.target.id === 'pestana1'){
+
+
+
+  }else if (e.target.id === 'pestana2'){
+  	document.getElementById("desayuno").style.display = 'none';
+  	document.getElementById("cena").style.display = 'none';
+  	document.getElementById("comida").style.display = 'block';
+  }else if(e.target.id === 'pestana3'){
+  	document.getElementById("desayuno").style.display = 'none';
+  	document.getElementById("comida").style.display = 'none';
+  	document.getElementById("cena").style.display = 'block';
 
   }
 };
