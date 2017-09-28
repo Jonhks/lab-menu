69d46b60efa2ef30f58a1aca5f67276a6182882d
memecast <amecgavante1305@gmail.com>
2017-09-27 20:34:21 -0500

    agregando condiciones para ocultar comida y cena

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,11 @@
 var mostrarOcultar = function(e){
   console.log(e.target);
-  if(e.target.id === "pestana1"){
-
+  if(e.target.id === 'pestana1'){
+    document.getElementById('comida').style.display='none';
+    document.getElementById('cena').style.display='none';
   }
 };
-var hola = document.getElementsByClassName("tab");
+var hola = document.getElementsByClassName('tab');
 for (var i = 0; i < hola.length; i++) {
-  hola[i].addEventListener("click",mostrarOcultar);
+  hola[i].addEventListener('click',mostrarOcultar);
 }
