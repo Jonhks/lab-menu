235be4612f55857a5ed0d8147f37efeb09cfa84a
memecast <amecgavante1305@gmail.com>
2017-09-27 20:19:19 -0500

    lol

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,11 @@
 var mostrarOcultar = function(e){
   console.log(e.target);
-  if(e.target.id === "pestana1")
+  if(e.target.id === "pestana1"){
+
+  }
 };
 var hola = document.getElementsByClassName("tab");
 for (var i = 0; i < hola.length; i++) {
   hola[i].addEventListener("click",mostrarOcultar);
 }
+c
